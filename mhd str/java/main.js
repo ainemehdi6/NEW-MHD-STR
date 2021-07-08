f52fec0a9c379ca28e52faef823313e561c4818e
EL MEHDI EL AINE <ainemehdi6@gmail.com>
2021-07-08 16:56:44 +0100

    Update main.js

--- mhd str/java/main.js
+++ mhd str/java/main.js
@@ -27,24 +27,3 @@ $(document).ready(function(){
 		$(this).parent(".result").empty();
 	});
 });
-
-const colors = document.querySelectorAll('.left-column .album span') ;
-const image = document.querySelector('.left-column .main-img img') ;
-const input = document.querySelector('.right-column .quantity input');
-const price = document.querySelector('.right-column .price') ;
-
-    input.addEventListener('change',()=> {
-        price.textContent = `USD ${input.value * 144.7}` ;
-	}) ;
-	colors[0].addEventListener('click' , ()=> {
-        image.src = 'images/s1.png' ;
-    }) ;
-    colors[1].addEventListener('click' , ()=> {
-        image.src = 'images/s3.png' ;
-    }) ;
-    colors[2].addEventListener('click' , ()=> {
-        image.src = 'images/s4.png' ;
-	}) ;
-	colors[3].addEventListener('click' , ()=> {
-        image.src = 'images/s5.png' ;
-    }) ;
